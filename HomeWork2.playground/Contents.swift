afa0589e242767a85cad56928cbe952789e11028
Bohdan <borysenkobohdan@gmail.com>
2023-04-24 15:28:48 +0300

    Розділ 1.2 - Змінні
    Оголосити 6 змінних із будь-якими значеннями для опису користувача
    для збeрігання наступної інформації (відповідні типи обрати самостійно)

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -11,3 +11,34 @@ print("Ім'я хатньої тварини: \(namePets)")
 print("Вага хатньої тварини: \(weight) кг")
 print("Вік хатньої тварини: \(age) років")
 print("Хатня тварина голодна: \(hungry) ")
+
+/* Розділ 1.2 - Змінні */
+
+var userName = "Bohdan"
+var serName = "Borysenko"
+var ageUser = 23
+var height = 1.83
+var weightUser = 73.5
+var marriage = false
+
+print("Інформація за 2018 р.:")
+print("Ім'я: \(userName)")
+print("Прізвище: \(serName)")
+print("Вік: \(ageUser)")
+print("зріст: \(height) см")
+print("Вага: \(weightUser) кг")
+print("У шлюбі: \(marriage)")
+
+var ageUser = 24
+var height = 1.85
+var marriage = true
+
+print("\nІнформація за поточний рік:")
+print("Інформація за 2018 р.:")
+print("Ім'я: \(userName)")
+print("Прізвище: \(serName)")
+print("Вік: \(ageUser)")
+print("зріст: \(height) см")
+print("Вага: \(weightUser) кг")
+print("У шлюбі: \(marriage)")
+
