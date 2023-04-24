1231a464abad79e138d4eda223b70a2d7b6abcb6
Bohdan <borysenkobohdan@gmail.com>
2023-04-24 15:07:21 +0300

    Константи
    1 Оголосити чотири константи та задати одразу будь-які значення
    використовуючи опис нижче для кожної константи:

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/* Розділ 1.1 - Константи */
+
+let namePets = "Mars"
+let weight = 5.6
+let age = 3
+let hungry = false
+
+print("Ім'я хатньої тварини: \(namePets)")
+print("Вага хатньої тварини: \(weight) кг")
+print("Вік хатньої тварини: \(age) років")
+print("Хатня тварина голодна: \(hungry) ")
