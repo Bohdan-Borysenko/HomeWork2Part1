f6def4dc59cd70dab1e383e23820699d48c81602
Bohdan <borysenkobohdan@gmail.com>
2023-04-24 16:36:47 +0300

    1.3-1.4

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -12,7 +12,7 @@ print("Вага хатньої тварини: \(weight) кг")
 print("Вік хатньої тварини: \(age) років")
 print("Хатня тварина голодна: \(hungry) ")
 
-/* Розділ 1.2 - Змінні */
+// Розділ 1.2 - Змінні
 
 var userName = "Bohdan"
 var serName = "Borysenko"
@@ -29,16 +29,46 @@ print("зріст: \(height) см")
 print("Вага: \(weightUser) кг")
 print("У шлюбі: \(marriage)")
 
-var ageUser = 24
-var height = 1.85
-var marriage = true
+//var ageUser = 24
+//var height = 1.85
+//var marriage = true
+//
+//print("\nІнформація за поточний рік:")
+//print("Інформація за 2018 р.:")
+//print("Ім'я: \(userName)")
+//print("Прізвище: \(serName)")
+//print("Вік: \(ageUser)")
+//print("зріст: \(height) см")
+//print("Вага: \(weightUser) кг")
+//print("У шлюбі: \(marriage)")
 
-print("\nІнформація за поточний рік:")
-print("Інформація за 2018 р.:")
-print("Ім'я: \(userName)")
-print("Прізвище: \(serName)")
-print("Вік: \(ageUser)")
-print("зріст: \(height) см")
-print("Вага: \(weightUser) кг")
-print("У шлюбі: \(marriage)")
+/* Розділ 1.3 - Псевдоніми */
 
+//typealias Humidity = `int
+
+var pressure = 1020
+var humidity = 50
+var temperature = 18
+
+print("Погода вчора:")
+print("Тиск: \(pressure) hPa")
+print("Вологість: \(humidity)%")
+print("Температура: \(temperature) C")
+//
+//var pressure = 1050
+//var humidity = 20
+//var temperature = 25
+//
+//print("Погода сьогодні:")
+//print("Тиск: \(pressure) hPa")
+//print("Вологість: \(humidity)%")
+//print("Температура: \(temperature) C")
+
+/* Розділ 1.4 - Кортежі */
+
+var profileInfo = (firstName: userName, serName: serName, age: ageUser, height: height, weightUser: weightUser, marriage: marriage)
+var weatherInfo = (pressure: pressure, humidity: humidity, temperature: temperature)
+print("Інфорація профіля:")
+print("Ім'я: \(profileInfo.firstName)", "Прізвище: \(profileInfo.serName)", "Вік: \(profileInfo.age)", "Зріст: \(profileInfo.height)", "Вага: \(profileInfo.weightUser)", "У шлюбі: \(profileInfo.marriage)")
+print("Інфорація про погоду:")
+print("Тиск: \(weatherInfo.pressure) hPa", "Вологість: \(weatherInfo.humidity) %", "Температура: \(weatherInfo.temperature) C")
